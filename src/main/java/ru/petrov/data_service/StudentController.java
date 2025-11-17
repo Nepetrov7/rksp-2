202447710fc815e2d77e256acef17d06a31388dd
n.petrov <n.petrov@dcoa.ru>
2025-11-17 13:47:15 +0300

    controller add

--- src/main/java/ru/petrov/data_service/StudentController.java
+++ src/main/java/ru/petrov/data_service/StudentController.java
@@ -1,5 +1,26 @@
-package ru.petrov.data_service;
+@RestController
+@RequiredArgsConstructor
+public class StudentController implements StudentDataApi {
 
-public class StudentController {
+  private final StudentRepository studentRepository;
+
+  @Override
+  public ResponseEntity<StudentDataResponse> createStudentDataInData(StudentDataCreateRequest request) {
+    Student student = new Student();
+    student.setName(request.getFullName());
+    student.setPassport(request.getPassport());
+    studentRepository.save(student);
+    StudentDataResponse response = new StudentDataResponse();
+    response.setId(student.getId());
+    response.setFullName(student.getName());
+    response.setPassport(student.getPassport());
+
+    return ResponseEntity.status(200).body(response);
+  }
+
+  /*@Override
+  public ResponseEntity<StudentDataResponse> getStudentDataByIdFromData(Long id) {
     
+    return ResponseEntity.status(200).body(response);
+  }*/
 }

b9fdd4c1e6ae9a593880dc271a403a192af605ab
n.petrov <n.petrov@dcoa.ru>
2025-11-19 14:04:36 +0300

    fix studentDataApi

--- gate/src/main/java/ru/petrov/gate/FeignClientConfig.java
+++ gate/src/main/java/ru/petrov/gate/FeignClientConfig.java
@@ -1,12 +1,16 @@
 package ru.petrov.gate;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import ru.petrov.gate.client.api.StudentDataApi;
+import ru.petrov.gate.client.ApiClient;
+
 @Configuration
 public class FeignClientConfig {
   @Bean
-  public SomeStudentApi someStudentApi() {
+  public StudentDataApi studentDataApi() {
     ApiClient apiClient = new ApiClient();
     apiClient.setBasePath("http://localhost:8083"); // или data.service.url
-    return apiClient.buildClient(SomeStudentApi.class);
+    return apiClient.buildClient(StudentDataApi.class);
   }
 }
